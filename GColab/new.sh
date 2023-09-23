66ed87eab7958012e7bc57b4dc70f0b40a4242f3
Tina H Wes <121371083+TinaHWes@users.noreply.github.com>
2023-09-23 10:22:38 +0000

    Update new.sh

--- GColab/new.sh
+++ GColab/new.sh
@@ -12,5 +12,5 @@ sudo apt install qemu-system-x86 curl -y > /dev/null 2>&1
 echo Your VNC IP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "Note: Use Right-Click Or Ctrl+C To Copy"
-sudo qemu-system-x86_64 -vnc :0 -hda w10x64.img  -smp cores=4  -m 8192M -machine usb=on -device usb-tablet > /dev/null 2>&1
+sudo qemu-system-x86_64 -vnc :0 -hda w7x64.img  -smp cores=4  -m 8192M -machine usb=on -device usb-tablet > /dev/null 2>&1
 seq 1 43200 | while read i; do echo -en "\r Running .     $i s /43200 s";sleep 0.1;echo -en "\r Running ..    $i s /43200 s";sleep 0.1;echo -en "\r Running ...   $i s /43200 s";sleep 0.1;echo -en "\r Running ....  $i s /43200 s";sleep 0.1;echo -en "\r Running ..... $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1;echo -en "\r Running  .... $i s /43200 s";sleep 0.1;echo -en "\r Running   ... $i s /43200 s";sleep 0.1;echo -en "\r Running    .. $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1; done
