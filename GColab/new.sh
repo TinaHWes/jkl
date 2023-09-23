186da0ba1483460d08d7fb0427def19db6e25df6
Tina H Wes <121371083+TinaHWes@users.noreply.github.com>
2023-09-23 11:01:20 +0000

    Update new.sh

--- GColab/new.sh
+++ GColab/new.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-curl -L -o w7x64.img https://bit.ly/akuhnetw7X64
+wget -O w7x64.img https://bit.ly/akuhnetw7X64
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 read -p "Paste authtoken here (Copy and Ctrl+V to paste then press Enter): " CRP
