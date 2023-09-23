26b282528414f6b58f82a93c0944e397cf69fa9d
Tina H Wes <121371083+TinaHWes@users.noreply.github.com>
2023-09-23 07:57:06 +0000

    Update new.sh

--- GColab/new.sh
+++ GColab/new.sh
@@ -1,3 +1,4 @@
+#! /bin/bash
 curl -L -o w7x64.img https://bit.ly/akuhnetw7X64
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
