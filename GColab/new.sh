70def4f41c5eb3538c259b4d5a49ed777bac1664
Tina H Wes <121371083+TinaHWes@users.noreply.github.com>
2023-09-23 13:45:51 +0000

    Update new.sh

--- GColab/new.sh
+++ GColab/new.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-wget -O w7x64.qcow2 https://bit.ly/akuhnetw7X64
+wget -O w7x64.qcow2 https://shorturl.at/kxAE2
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 read -p "Paste authtoken here (Copy and Ctrl+V to paste then press Enter): " CRP
@@ -12,6 +12,6 @@ sudo apt install qemu-system-x86 curl -y > /dev/null 2>&1
 echo Your VNC IP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "Note: Use Right-Click Or Ctrl+C To Copy"
-sudo qemu-system-x86_64 -vnc :0 -hda w7x64.qcow2  -smp cores=2  -m 8192M -machine usb=on -device usb-tablet > /dev/null 2>&1
+sudo qemu-system-x86_64 -vnc :0 -hda w7x64.qcow2  -smp cores=2  -m 4G -machine usb=on -device usb-tablet > /dev/null 2>&1
 sleep 43210
 seq 1 43200 | while read i; do echo -en "\r Running .     $i s /43200 s";sleep 0.1;echo -en "\r Running ..    $i s /43200 s";sleep 0.1;echo -en "\r Running ...   $i s /43200 s";sleep 0.1;echo -en "\r Running ....  $i s /43200 s";sleep 0.1;echo -en "\r Running ..... $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1;echo -en "\r Running  .... $i s /43200 s";sleep 0.1;echo -en "\r Running   ... $i s /43200 s";sleep 0.1;echo -en "\r Running    .. $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1; done
